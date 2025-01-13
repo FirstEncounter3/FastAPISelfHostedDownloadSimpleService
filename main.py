d110ecee6dd9e45888b7b640c346285210553c0f
First_Encounter <needenergy27@gmail.com>
2025-01-14 00:08:35 +0700

    separate runner

--- main.py
+++ main.py
@@ -2,17 +2,10 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from routes.router import router
 
-import uvicorn
-
-from config import PORT
 from utilities.file_and_directory_handler import create_download_and_upload_directory
 
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 app.include_router(router=router)
-
-
-if __name__ == "__main__":
-    create_download_and_upload_directory()
-    uvicorn.run(app, host="0.0.0.0", port=PORT)
+create_download_and_upload_directory()
